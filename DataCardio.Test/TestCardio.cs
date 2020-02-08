bb2aa399fb132edfc1ca025c1ce826d6a439c4c1
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-02-08 11:41:30 +0100

    .

---
+++ DataCardio.Test/TestCardio.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataCardio.Test
+{
+    [TestClass]
+    public class TestCardio
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ DataCardio/DataCardio.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DataCardio
+{
+    public class DataCardio
+    {
+    }
+}
