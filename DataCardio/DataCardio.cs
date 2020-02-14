60c88b40d40219dbed023788a76d0e2a4158916b
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-02-14 13:29:55 +0100

    aggiunto battiti allenamento

--- DataCardio/DataCardio.cs
+++ DataCardio/DataCardio.cs
@@ -4,6 +4,24 @@ namespace DataCardio
 {
     public class DataCardio
     {
+        public static int CalcoloBattiti (int eta)
+        {
+            int battitimax = 220 - eta;
+            return battitimax;
+        }
+        public static string Allenamento(int BattitiAttuali) 
+        {
+            int AllEffMin = BattitiAttuali / 100 * 70;
+            int AllEffMax = BattitiAttuali / 100 * 90;
+            string risposta;
+            if (BattitiAttuali<= AllEffMax && BattitiAttuali>= AllEffMin)
+            {
+                return risposta = "ti stai allenando bene!";
+            }else
+            {
+                return risposta = $"Ti stai allenando male! Per allenarti bene devi stare tra {AllEffMin} e {AllEffMax}.";
+            }
+        }
         public static string  FrequenzaCardiacaRiposo(int battiti)
         {
             if (battiti<60)
