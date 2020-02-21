8f70698a4718ed35a18db8d67677c003aa492176
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-02-21 13:30:20 +0100

    punto 3

--- DataCardio.Test/TestCardio.cs
+++ DataCardio.Test/TestCardio.cs
@@ -8,7 +8,43 @@ namespace DataCardio.Test
         [TestMethod]
         public void TestCardio1()
         {
-            
+            int età = 26;
+            int asp = 194;
+            int risp = DataCardio.CalcoloBattiti(età);
+            Assert.AreEqual(asp, risp);
+        }
+        [TestMethod]
+        public void TestAllenamento()
+        {
+            double BattitiAttuali = 165;
+            double BattitiMax = 194;
+            string asp = "Ti stai allenando bene!";
+            string risp = DataCardio.Allenamento(BattitiAttuali, BattitiMax);
+            Assert.AreEqual(asp, risp);
+        }
+        [TestMethod]
+        public void TestFrequenzaCardiacaRiposo()
+        {
+            int battiti = 80;
+            string asp = "normale";
+            string risp = DataCardio.FrequenzaCardiacaRiposo(battiti);
+            Assert.AreEqual(asp, risp);
+
+        }
+        [DataTestMethod]
+        [DataRow("uomo",18,80,30,165,491.53)]
+        [DataRow("donna", 18, 70, 30, 165, 455.57)]
+
+        public void TestCalorieBruciate(string sesso, double anni, double peso, double tempo, double freq,double asp)
+        {
+            //string sesso = "uomo";
+            //double anni = 18;
+            //double peso = 80;
+            //double freq = 165;
+            //double tempo = 30;
+            //double asp = 491.53;
+            double risp =DataCardio.CalorieBruciate(sesso, anni, peso, tempo, freq);
+            Assert.AreEqual(asp, risp);
         }
     }
 }
--- DataCardio/DataCardio.cs
+++ DataCardio/DataCardio.cs
@@ -9,18 +9,19 @@ namespace DataCardio
             int battitimax = 220 - eta;
             return battitimax;
         }
-        public static string Allenamento(int BattitiAttuali) 
+        public static string Allenamento(double BattitiAttuali, double BattitiMax) 
         {
-            int AllEffMin = BattitiAttuali / 100 * 70;
-            int AllEffMax = BattitiAttuali / 100 * 90;
-            string risposta;
-            if (BattitiAttuali<= AllEffMax && BattitiAttuali>= AllEffMin)
+            double AllEffMin = BattitiMax / 100 * 70;
+            double AllEffMax = BattitiMax / 100 * 90;
+            string risposta = "";
+            if (BattitiAttuali < AllEffMax && BattitiAttuali > AllEffMin)
             {
-                return risposta = "ti stai allenando bene!";
+                risposta = "Ti stai allenando bene!";
             }else
             {
-                return risposta = $"Ti stai allenando male! Per allenarti bene devi stare tra {AllEffMin} e {AllEffMax}.";
+                risposta = "Ti stai allenando male!";
             }
+            return risposta;
         }
         public static string  FrequenzaCardiacaRiposo(int battiti)
         {
@@ -38,5 +39,21 @@ namespace DataCardio
                 return "tachicardico";
             }
         }
+
+        public static double CalorieBruciate(string sesso,double anni, double peso, double tempo,double freq)
+        {
+            double calorie=0;
+            if (sesso == "uomo")
+            {
+                calorie = ((anni * 0.2017) + (peso * 0.199) + (freq * 0.6309) - 55.0969) * tempo / 4.184;                
+            }
+            else if(sesso == "donna")
+            {
+                calorie = ((anni * 0.074) + (peso * 0.126) + (freq * 0.4472) - 20.4022) * tempo / 4.184;
+            }
+            calorie = Math.Round(calorie, 2);
+            return calorie;
+        }
+
     }
 }
